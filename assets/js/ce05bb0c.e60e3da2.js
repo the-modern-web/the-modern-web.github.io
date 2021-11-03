c1c950adf13df0d7508ccdbc8414518c9def92dc
mehdi985 <mehdi985@gmail.com>
2021-11-03 19:15:46 +0400

    Deploy website - based on 41dab7b6da6d2ba77bb3216abd8a2c0ab6964850

---
+++ assets/js/ce05bb0c.e60e3da2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[1683],{3774:function(e){e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"greetings","name":"greetings","count":1,"permalink":"/blog/tags/greetings"}')}}]);
